0a40080f05528292c705b5ea7ff2e3dacb85fe79
Prabin Rath <prabinrath123@gmail.com>
2022-11-11 01:59:11 -0700

    some un successful attempts

--- source/2.multiagent/multiAgents.py
+++ source/2.multiagent/multiAgents.py
@@ -19,73 +19,29 @@ import math
 
 from game import Agent
 
-class ReflexAgent(Agent):
-    """
-    A reflex agent chooses an action at each choice point by examining
-    its alternatives via a state evaluation function.
-
-    The code below is provided as a guide.  You are welcome to change
-    it in any way you see fit, so long as you don't touch our method
-    headers.
-    """
-
-
-    def getAction(self, gameState):
-        """
-        You do not need to change this method, but you're welcome to.
-
-        getAction chooses among the best options according to the evaluation function.
-
-        Just like in the previous project, getAction takes a GameState and returns
-        some Directions.X for some X in the set {NORTH, SOUTH, WEST, EAST, STOP}
-        """
-        # Collect legal moves and successor states
-        legalMoves = gameState.getLegalActions()
-
-        # Choose one of the best actions
-        scores = [self.evaluationFunction(gameState, action) for action in legalMoves]
-        bestScore = max(scores)
-        bestIndices = [index for index in range(len(scores)) if scores[index] == bestScore]
-        chosenIndex = random.choice(bestIndices) # Pick randomly among the best
-
-        "Add more of your code here if you want to"
-
-        return legalMoves[chosenIndex]
-
-    def evaluationFunction(self, currentGameState, action):
-        """
-        Design a better evaluation function here.
-
-        The evaluation function takes in the current and proposed successor
-        GameStates (pacman.py) and returns a number, where higher numbers are better.
-
-        The code below extracts some useful information from the state, like the
-        remaining food (newFood) and Pacman position after moving (newPos).
-        newScaredTimes holds the number of moves that each ghost will remain
-        scared because of Pacman having eaten a power pellet.
-
-        Print out these variables to see what you're getting, then combine them
-        to create a masterful evaluation function.
-        """
-        # Useful information you can extract from a GameState (pacman.py)
-        successorGameState = currentGameState.generatePacmanSuccessor(action)
-        newPos = successorGameState.getPacmanPosition()
-        newFood = successorGameState.getFood()
-        newGhostStates = successorGameState.getGhostStates()
-        newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
-
-        "*** YOUR CODE HERE ***"
-        return successorGameState.getScore()
+def euclidianDistance(x1,x2):
+    return ( (x1[0] - x2[0]) ** 2 + (x1[1] - x2[1]) ** 2 ) ** 0.5
 
 def scoreEvaluationFunction(currentGameState):
-    """
-    This default evaluation function just returns the score of the state.
-    The score is the same one displayed in the Pacman GUI.
+    newPos = currentGameState.getPacmanPosition()
+    newFood = currentGameState.getFood()
+    # newGhostStates = currentGameState.getGhostStates()
+    # # newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
+    food_proximity = 0
+    for food in newFood.asList():
+        food_proximity += manhattanDistance(food, newPos)
+    # ghost_proximity = 0
+    # for ghost in newGhostStates:
+    #     ghost_proximity += euclidianDistance(ghost.getPosition(), newPos)
 
-    This evaluation function is meant for use with adversarial search agents
-    (not reflex agents).
-    """
-    return currentGameState.getScore()
+    # utility = currentGameState.getScore()
+    utility=0    
+    if currentGameState.isWin():
+        utility=1000    
+    if currentGameState.isLose():
+        utility=0
+    utility -= food_proximity
+    return utility
 
 class MultiAgentSearchAgent(Agent):
     """
@@ -106,64 +62,6 @@ class MultiAgentSearchAgent(Agent):
         self.index = 0 # Pacman is always agent index 0
         self.evaluationFunction = util.lookup(evalFn, globals())
         self.depth = int(depth)
-
-class MinimaxAgent(MultiAgentSearchAgent):
-    """
-    Your minimax agent (question 2)
-    """
-
-    def getAction(self, gameState):
-        """
-        Returns the minimax action from the current gameState using self.depth
-        and self.evaluationFunction.
-
-        Here are some method calls that might be useful when implementing minimax.
-
-        gameState.getLegalActions(agentIndex):
-        Returns a list of legal actions for an agent
-        agentIndex=0 means Pacman, ghosts are >= 1
-
-        gameState.generateSuccessor(agentIndex, action):
-        Returns the successor game state after an agent takes an action
-
-        gameState.getNumAgents():
-        Returns the total number of agents in the game
-
-        gameState.isWin():
-        Returns whether or not the game state is a winning state
-
-        gameState.isLose():
-        Returns whether or not the game state is a losing state
-        """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
-
-class AlphaBetaAgent(MultiAgentSearchAgent):
-    """
-    Your minimax agent with alpha-beta pruning (question 3)
-    """
-
-    def getAction(self, gameState):
-        """
-        Returns the minimax action using self.depth and self.evaluationFunction
-        """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
-
-class ExpectimaxAgent(MultiAgentSearchAgent):
-    """
-      Your expectimax agent (question 4)
-    """
-
-    def getAction(self, gameState):
-        """
-        Returns the expectimax action using self.depth and self.evaluationFunction
-
-        All ghosts should be modeled as choosing uniformly at random from their
-        legal moves.
-        """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
 
 class MCTSNode:
     def __init__(self, state, parent_action=None, ucb_param=2, max_sim_steps = 100, eval_fun=None):
@@ -187,8 +85,11 @@ class MCTSNode:
                 # Expand
                 self.expand()
 
-        # Selection        
-        value = self.children[self.select()].iterate()
+        # Selection     
+        if len(self.children) == 0:
+            value = self.rollout()
+        else:   
+            value = self.children[self.select()].iterate()
 
         # Backpropagation
         self.avg_value += value
