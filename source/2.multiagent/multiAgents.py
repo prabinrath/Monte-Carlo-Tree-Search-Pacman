aa1d4d0d6d24760a1d8149ffdae023931d85eda7
rahilhastu <rahilhastu@gmail.com>
2022-11-12 20:42:29 -0700

    merging with mcts-custom

--- source/2.multiagent/multiAgents.py
+++ source/2.multiagent/multiAgents.py
@@ -16,14 +16,120 @@ from util import manhattanDistance
 from game import Directions
 import random, util
 import math
-from featureExtractors import *
-from statistics import mean
-
 from game import Agent
 
+class ReflexAgent(Agent):
+    """
+    A reflex agent chooses an action at each choice point by examining
+    its alternatives via a state evaluation function.
+
+    The code below is provided as a guide.  You are welcome to change
+    it in any way you see fit, so long as you don't touch our method
+    headers.
+    """
+
+
+    def getAction(self, gameState):
+        """
+        You do not need to change this method, but you're welcome to.
+
+        getAction chooses among the best options according to the evaluation function.
+
+        Just like in the previous project, getAction takes a GameState and returns
+        some Directions.X for some X in the set {NORTH, SOUTH, WEST, EAST, STOP}
+        """
+        # Collect legal moves and successor states
+        legalMoves = gameState.getLegalActions()
+        # print(legalMoves)
+        # Choose one of the best actions
+        scores = [self.evaluationFunction(gameState, action) for action in legalMoves]
+        # print(scores)
+        bestScore = max(scores)
+        bestIndices = [index for index in range(len(scores)) if scores[index] == bestScore]
+        chosenIndex = random.choice(bestIndices) # Pick randomly among the best
+
+        "Add more of your code here if you want to"
+        # print(legalMoves[chosenIndex])
+        return legalMoves[chosenIndex]
+
+    def evaluationFunction(self, currentGameState, action):
+        """
+        Design a better evaluation function here.
+
+        The evaluation function takes in the current and proposed successor
+        GameStates (pacman.py) and returns a number, where higher numbers are better.
+
+        The code below extracts some useful information from the state, like the
+        remaining food (newFood) and Pacman position after moving (newPos).
+        newScaredTimes holds the number of moves that each ghost will remain
+        scared because of Pacman having eaten a power pellet.
+
+        Print out these variables to see what you're getting, then combine them
+        to create a masterful evaluation function.
+        """
+        # Useful information you can extract from a GameState (pacman.py)
+        # x, y = currentGameState.getPacmanPosition()
+        # print(currentGameState.getGhostPosition(x))
+        # print(action)
+        successorGameState = currentGameState.generatePacmanSuccessor(action)
+        # print(successorGameState)
+        newPos = successorGameState.getPacmanPosition()
+        # print(newPos)
+        newFood = successorGameState.getFood()
+        # print(newFood.asList())
+        newGhostStates = successorGameState.getGhostStates()
+        # print(successorGameState.getGhostPositions())
+        newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
+        # print(newScaredTimes)
+        # exit()
+
+        "*** YOUR CODE HERE ***"
+        pacmanPos = newPos
+        minDistanceFood2Pacman = []
+        maxDistanceGhost2Pacman = []
+        score =0
+        if len(newFood.asList()) ==0:
+            return successorGameState.getScore()*10
+        for foodPosition in newFood.asList():
+            # minDistanceFood2Pacman.append(math.dist(foodPosition, pacmanPos))
+            minDistanceFood2Pacman.append(util.manhattanDistance(foodPosition, pacmanPos))
+            # print("Minimum Distance:"+str(minDistanceFood2Pacman))
+        minDistF2P = min(minDistanceFood2Pacman)
+
+        for ghostPosition in successorGameState.getGhostPositions():
+            # maxDistanceGhost2Pacman.append(math.dist(ghostPosition, pacmanPos))
+            maxDistanceGhost2Pacman.append(util.manhattanDistance(ghostPosition, pacmanPos))
+        maxDistG2P = max(maxDistanceGhost2Pacman)
+        # print("====")
+        # print(minDistF2P, maxDistG2P)
+        if newScaredTimes[0]==0:
+            score = 0
+        else:
+            score +=10
+
+        if action=="Stop":
+            score-=10
+        # print("Max: "+str(maxDistG2P))
+        # print("Min: "+str(minDistF2P))
+        # print("Score "+str(successorGameState.getScore()))
+        # print("Max/min: "+str((maxDistG2P/minDistF2P)*3 ))
+        # print("\n\=======\n")
+        # exit()
+        try:
+            div = (minDistF2P/maxDistG2P)**-1
+        except:
+            div =0
+        return successorGameState.getScore() + div +score
+
 def scoreEvaluationFunction(currentGameState):
-    utility = currentGameState.getScore()
-    return utility
+    """
+    This default evaluation function just returns the score of the state.
+    The score is the same one displayed in the Pacman GUI.
+
+    This evaluation function is meant for use with adversarial search agents
+    (not reflex agents).
+    """
+    return currentGameState.getScore()
 
 class MultiAgentSearchAgent(Agent):
     """
@@ -46,7 +152,9 @@ class MultiAgentSearchAgent(Agent):
         self.depth = int(depth)
 
 class MinimaxAgent(MultiAgentSearchAgent):
-
+    """
+    Your minimax agent (question 2)
+    """
     def getAction(self, gameState):
         """
         Returns the minimax action from the current gameState using self.depth
@@ -109,9 +217,13 @@ class MinimaxAgent(MultiAgentSearchAgent):
         ans = valueMinMax(gameState, 0, self.index)
         return ans[1]
         
-        # util.raiseNotDefined()
+        util.raiseNotDefined()
+    
 
 class AlphaBetaAgent(MultiAgentSearchAgent):
+    """
+    Your minimax agent with alpha-beta pruning (question 3)
+    """
 
     def getAction(self, gameState):
         """
@@ -166,9 +278,12 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
         beta = 99999999
         ans = valueMinMax(gameState, 0, self.index, alpha, beta)
         return ans[1]
-        # util.raiseNotDefined()
+        util.raiseNotDefined()
 
 class ExpectimaxAgent(MultiAgentSearchAgent):
+    """
+      Your expectimax agent (question 4)
+    """
 
     def getAction(self, gameState):
         """
@@ -225,25 +340,7 @@ class ExpectimaxAgent(MultiAgentSearchAgent):
         ans = valueMinMax(gameState, 0, self.index, alpha, beta)
         return ans[1]
 
-        # util.raiseNotDefined()
-
-class MultiAgentSearchAgent(Agent):
-    """
-    This class provides some common elements to all of your
-    multi-agent searchers.  Any methods defined here will be available
-    to the MinimaxPacmanAgent, AlphaBetaPacmanAgent & ExpectimaxPacmanAgent.
-    You *do not* need to make any changes here, but you can if you want to
-    add functionality to all your adversarial search agents.  Please do not
-    remove anything, however.
-    Note: this is an abstract class: one that should not be instantiated.  It's
-    only partially specified, and designed to be extended.  Agent (game.py)
-    is another abstract class.
-    """
-
-    def __init__(self, evalFn = 'scoreEvaluationFunction', depth = '2'):
-        self.index = 0 # Pacman is always agent index 0
-        self.evaluationFunction = util.lookup(evalFn, globals())
-        self.depth = int(depth)
+        util.raiseNotDefined()
 
 class MCTSNode:
     def __init__(self, state, parent_action=None, ucb_param=2, max_sim_steps = 50):
